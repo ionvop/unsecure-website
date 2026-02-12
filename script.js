b49e797079884ac9c75dea4dc66a36494f78573c
ionvop <ionvop@gmail.com>
2026-02-12 11:39:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const g_inputSearch = document.getElementById("g_inputSearch");
-initialize();
+g_initialize();
 
-function initialize() {
+function g_initialize() {
+    for (let element of document.querySelectorAll("*")) {
+        element.style.setProperty("--test", "test");
+        element.style.removeProperty("--test");
+    }
+
     for (let element of document.querySelectorAll(".unix")) {
         element.innerHTML = new Date(element.innerHTML * 1000).toLocaleString();
     }
